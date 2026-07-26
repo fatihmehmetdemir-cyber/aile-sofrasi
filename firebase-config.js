06843f8e97d9f71da9c377afdc783d65ffc5aa14
fatihmehmetdemir-cyber <fatihmehmetdemir@gmail.com>
2026-07-26 16:49:11 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDEzA8qINph9PyzI2iSfhC7gl5QMNV1Mhg",
+  authDomain: "aile-sofra.firebaseapp.com",
+  projectId: "aile-sofra",
+  storageBucket: "aile-sofra.firebasestorage.app",
+  messagingSenderId: "998355782423",
+  appId: "1:998355782423:web:2896e8ad3a0ceffdb74552",
+  measurementId: "G-PJRMJ0WDNF"
+};
