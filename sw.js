4723d014d0a527ccfa4366e9b990d88af7dd52cb
fatihmehmetdemir-cyber <fatihmehmetdemir@gmail.com>
2026-07-26 22:22:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aile-sofrasi-v037-special-events';
+const CACHE='aile-sofrasi-v038-correct-measures';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./firebase-config.js','./appcheck-config.js'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
